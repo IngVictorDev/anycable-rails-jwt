d3317c0b23c9e5599395054e15cd8bf1cd24b215
Victor <victor.h.mtz.hdz@outlook.com>
2025-04-23 17:42:13 -0600

    wip

--- lib/anycable-rails-jwt.rb
+++ lib/anycable-rails-jwt.rb
@@ -15,21 +15,21 @@ module AnyCable
       class << self
         def encode(expires_at: nil, **identifiers)
           key = AnyCable.config.jwt_id_key
-          raise ArgumentError, "JWT encryption key is not specified. Add it via `jwt_id_key` option" if key.nil? || key.empty?
+          raise ArgumentError, "JWT encryption key is not specified. Add it via `jwt_id_key` option" if key.blank?
 
           expires_at ||= AnyCable.config.jwt_id_ttl.seconds.from_now
 
-          payload = {
-            ext: identifiers,
-            exp: expires_at.to_i
-          }
+          serialized_ids = identifiers.transform_values do |v|
+            v.is_a?(ActiveRecord::Base) ? v.id : v
+          end
 
+          payload = {ext: serialized_ids, exp: expires_at.to_i}
           ::JWT.encode(payload, key, ALGORITHM)
         end
 
         def decode(token)
           key = AnyCable.config.jwt_id_key
-          raise ArgumentError, "JWT encryption key is not specified. Add it via `jwt_id_key` option" if key.nil? || key.empty?
+          raise ArgumentError, "JWT encryption key is not specified. Add it via `jwt_id_key` option" if key.blank?
 
           decoded_token = ::JWT.decode(token, key, true, algorithm: ALGORITHM)
           decoded_token.first["ext"]
